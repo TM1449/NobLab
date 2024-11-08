1f2a6510e5af1ce03f38f52dd8bb489e5d0eca1f
TM1449_Lab <tnkmsk49@gmail.com>
2024-11-08 16:45:31 +0900

    Add 2024_1108_1645

--- Graphical/TSTSTSTST.py
+++ Graphical/TSTSTSTST.py
@@ -1,19 +1,25 @@
 import numpy as np
 from scipy import optimize
+import scipy.linalg
 
-a = 0.89
-b = 0.6
-c = 0.28
-k_0 = 0.04
+a = 0.5
+b = 0.4
+c = 0.89
+k_0 = -0.44
 k_1 = 0.1
 k_2 = 0.2
 alpha = 0.1
-beta = 0.2
-k = -3.2
+beta = 0.1
+k = 4.1703
 
-x = 0.0448
+x = 0.831
 
-y = (-b * x + c) / (1-a)
-phi = k_1 * x / (1+k_2)
+y = round((-b * x + c) / (1-a),4)
+phi = round(k_1 * x / (1+k_2),4)
 
 print(x,y,phi)
+
+A = np.array([[np.exp(y-x) * (2*x - x**2) + k * (alpha + 3 * beta*phi**2), x**2*np.exp(y-x), 6*k*x*beta*phi], [-1*b, a, 0],[k_1,0,-1*k_2]])
+val = scipy.linalg.eigvals(A)
+
+print(val)
